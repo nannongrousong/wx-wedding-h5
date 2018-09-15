b2ef316dee3992d8a90718999e3817db062b9fc6
nannongrousong <yuanwansong@hotmail.com>
2018-09-16 07:57:29 +0800

    init

--- src/js/container/PictureWall.js
+++ src/js/container/PictureWall.js
@@ -1,8 +1,24 @@
 import React, { Component, Fragment } from 'react';
-import PictureWall from '@Component/PictureWall2'
+import PictureWall from '@Component/PictureWall'
 import BottomToolbar from '@Component/BottomToolbar'
 
 export default class extends Component {
+    componentDidMount() {
+        this.scroll()
+    }
+
+    scroll = () => {
+        document.documentElement.scrollTop += 3;
+        if (document.documentElement.scrollTop + 1000 > document.documentElement.scrollHeight) {
+            document.documentElement.scrollTop = 0;
+        }
+        this.frameID = window.requestAnimationFrame(this.scroll)
+    }
+
+    componentWillUnmount() {
+        window.cancelAnimationFrame(this.frameID)
+    }
+
     render() {
         return (
             <Fragment>
